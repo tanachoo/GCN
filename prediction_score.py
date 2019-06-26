103840dc9f90ff8d29c0d9f0897451c6b4993a8b
Yoshihisa Tanaka <yoshihisa.tnk04@gmail.com>
2019-06-26 14:42:18 +0900

    Update prediction_score.py

--- prediction_score.py
+++ prediction_score.py
@@ -5,7 +5,7 @@
 ## Script for converting prediction score to table
 
 # Usage: python prediction_score.py --result ./pool_result/target0_20190425/cv_info.gcn.jbl --dataset ./processed_data/target0_20190425/dataset.jbl \
-# --node ./processed_data/target0_20190425/dataset_node.csv --cv 0 --scorerank 15000 --output ./pool_result/target0_20190425/score.txt
+# --node ./processed_data/target0_20190425/dataset_node.csv --cv 0 --scorerank 15000 --train false --output ./pool_result/target0_20190425/score.txt
 
 import joblib
 import numpy as np
