45a51542931143078efea2ffbcb9e09d767a81eb
tanachoo <yoshihisa.tnk04@gmail.com>
2019-09-09 01:38:01 +0000

    revised

--- shortest_path.py
+++ shortest_path.py
@@ -1,17 +1,21 @@
 
 # Auther: yoshi
 # Date: 9/2/2019
+# Updated: 9/9/2019
 # Project: gcn pathaway
 
 ## script for calculate shortest path in Graph
+##Usage: python shortest_path.py --input ../result/main/ppi/target1/score_ip2_cv0.pkl --graph ./wo_target1.ppigraph.tsv --output ./score_path_ppi_target1_cv0_ip2.txt
 
 import pickle
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 import argparse
+import time
 
 def main():
+    start_time = time.time()
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', type=str, help="input: score_gcn2_cv0.pkl")
@@ -21,7 +25,7 @@ def main():
 
 
     ## load data
-    # d1 = './result/main/ppi/target0/score_gcn2_cv0.pkl'
+    # pkl data: score_gcn2_cv0.pkl
     with open(args.input, 'rb') as f:
         table = pickle.load(f)
     print(f'#table shape: {table.shape}\n')
@@ -30,14 +34,14 @@ def main():
     gene2 = table['gene2'].values.tolist()
 
     ## build graph
-    # input_graph = './test_graph_input.tsv'
-    G = nx.read_edgelist(args.graph, nodetype=str)
-    print(f'number_of_nodes: {nx.number_of_nodes(G)}\n'
+    # input_graph = './test_graph_input.tsv' (format: node tab node)
+    G = nx.read_edgelist(args.graph, nodetype=str, create_using=nx.MultiGraph())
+    print(f'load graph: {args.graph}\n'
+          f'number_of_nodes: {nx.number_of_nodes(G)}\n'
           f'number_of_edges: {nx.number_of_edges(G)}')
 
     ## calculate shortest path
     shortest_path = []
-
     for i in range(len(gene1)):
         source = gene1[i]
         target = gene2[i]
@@ -56,7 +60,9 @@ def main():
     with open(args.output, 'w') as f:
         table.to_csv(f, sep='\t', header=True, index=False)
 
-    print('-- fin --\n')
+    elapsed_time = time.time() - start_time
+    print(f'\n#time: {elasped_time} sec\n'
+          f'-- fin --\n')
 
 if __name__ == '__main__':
     main()
